1d6e2cebc3dca331664d218109c50b4fb781952c
noir-depresso <ryanliusixian@outlook.com>
2026-07-23 22:58:45 -0700

    Added a player with basic controls

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -1,11 +1,17 @@
-extends Node
+extends CharacterBody2D
+
+#speed i chose for now, maybe speed upgrades later?
+@export var speed: float = 250.0
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(_delta: float) -> void:
+	#get the input and set the velocity, then move.
+	var direction := Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	velocity = direction * speed
+	move_and_slide()
