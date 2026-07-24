a50a322157d0157ced68f3137da2017ecdd01aee
noir-depresso <ryanliusixian@outlook.com>
2026-07-23 23:12:10 -0700

    ai now tracks player directly
    no more obstacle nav.

--- Scripts/ai.gd
+++ Scripts/ai.gd
@@ -1,42 +1,13 @@
 extends CharacterBody2D
 
-# This was a small AI pathfinding script I wrote. Feel free to change parts as this is just showing it works.
-# Even though the navigation is displayed on a 2D flat tilemap, it works for orthographic art as well.
-# the reason for this script to make little guys wander around the map while avoiding trees or buildings or whatever we come up with
+@export var speed: float = 100.0
+@export var player: CharacterBody2D
 
-var speed = 100.0
-var has_reached_target_pos = false
-var target_pos : Vector2
+func _physics_process(_delta: float) -> void:
+	if player == null:
+		return
 
-@onready var navigation = $NavigationAgent2D
+	var direction = global_position.direction_to(player.global_position)
 
-
-func _physics_process(delta: float) -> void:
-	if !has_reached_target_pos: #Checks if target has not been reached, allowing movement
-		movement()
-
-	if navigation.target_position != target_pos: #Checks if the nav target position has been changed, and a new path must be created
-		has_reached_target_pos = false
-
-	update_path()
-
-	if Input.is_action_just_pressed("Left_Click"): #Temporary to show how the AI will go towards the mouse clicked position
-		print("Mouse Clicked")
-		target_pos = get_global_mouse_position()
-
-
-func movement():
-	var dir = to_local(navigation.get_next_path_position()).normalized()
-	velocity = dir * speed
+	velocity = direction * speed
 	move_and_slide()
-
-
-func update_path():
-	if target_pos != null:
-		navigation.target_position = target_pos
-
-
-func _on_navigation_agent_2d_navigation_finished() -> void: #Checks when the AI has reached the end of its path
-	has_reached_target_pos = true
-	print("Target Reached")
-	
